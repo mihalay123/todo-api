9ea988194b14f4b63f61520c492ce02e55e76b2a
mihalay123 <mihalay26@gmail.com>
2025-06-22 22:13:43 +0400

    add Swagger documentation and setup for ToDo API

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	_ "todo-api/docs"
 	"todo-api/todo"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// @title           ToDo API
+// @version         1.0
+// @description     Простое API для управления задачами
+// @host            localhost:8080
+// @BasePath        /
+
 func main() {
+	http.Handle("/docs/", httpSwagger.WrapHandler)
 	http.HandleFunc("/todos", todo.TodoHandler)
 	log.Println("Server started at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
